c1126df21bad86f2bbe5b89fe465586ee3f97654
Kai <610288425@qq.com>
2020-02-07 22:09:26 +0800

    css init

---
+++ src/vue.config.js
@@ -0,0 +1,14 @@
+module.export = {
+	configureWebpack: {
+		resolve: {
+			// extensions: [],
+			alias: {
+				assets: "@/assets",
+				common: "@/common",
+				components: "@/components",
+				network: "@/network",
+				views: "@/views"
+			}
+		}
+	}
+};
