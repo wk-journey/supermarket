525bdc3f772fb11954f0c2ef709221f4c1874b6e
Kai <610288425@qq.com>
2020-02-08 15:34:35 +0800

    navi add

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-	<div id="app"></div>
+  <div id="app">
+    <router-view />
+    <main-tab-bar />
+  </div>
 </template>
 
 <script>
-	export default {
-		name: "App",
-		components: {}
-	};
+import MainTabBar from "components/content/mainTabbar/MainTabBar";
+
+export default {
+  name: "App",
+  components: {
+    MainTabBar
+  }
+};
 </script>
 
 <style>
-	@import "assets/css/base.css";
+@import "assets/css/base.css";
 </style>
